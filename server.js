82631802a8f3ed2a99f4e3d89f2373e9fdbdac88
Arash <arash@JavascriptCoder.lan>
2015-12-18 17:55:13 +0800

    Morgan

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express');
+var morgan = require('morgan');
 
 var app = express();
+
+// Middleware
+app.use(morgan('dev'));
 
 app.get('/', function(req, res) {
   var name = "Batman";
   res.json("My name is " + name);
+});
+
+app.get('/catname', function(req, res) {
+  res.json('batman');
 });
 
 app.listen(3000, function(err) {
