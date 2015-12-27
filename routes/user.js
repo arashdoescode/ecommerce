0b286f06c800fd09921c964748b1b35037ffb944
Arash <arash@JavascriptCoder.local>
2015-12-28 02:00:39 +0800

    Signup Part 3

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,9 @@ var User = require('../models/user');
 
 
 router.get('/signup', function(req, res, next) {
-  res.render('accounts/signup');
+  res.render('accounts/signup', {
+    errors: req.flash('errors')
+  });
 });
 
 router.post('/signup', function(req, res, next) {
@@ -16,13 +18,13 @@ router.post('/signup', function(req, res, next) {
   User.findOne({ email: req.body.email }, function(err, existingUser) {
 
     if (existingUser) {
-      console.log(req.body.email + " is already exist");
+      req.flash('errors', 'Account with that email address already exists');
       return res.redirect('/signup');
     } else {
       user.save(function(err, user) {
         if (err) return next(err);
 
-        res.json("New user has been created");
+        return res.redirect('/');
       });
     }
   });
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var engine = require('ejs-mate');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var flash = require('express-flash');
 
 var User = require('./models/user');
 
@@ -22,6 +25,14 @@ app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: "Arash@$@!#@"
+}));
+app.use(flash());
+
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
