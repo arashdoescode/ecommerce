7837d7a94728fea052f2f902c700d313cd8f8197
Arash <arash@JavascriptCoder.local>
2015-12-29 00:07:32 +0800

    Login Part 2

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy();
-
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/user');
 
 // serialize and deserialize
 passport.serializeUser(function(user, done) {
@@ -12,7 +12,6 @@ passport.deserializeUser(function(id, done) {
     done(err, user);
   });
 });
-
 
 
 //Middleware
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
@@ -7,7 +8,7 @@ var engine = require('ejs-mate');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var flash = require('express-flash');
-var MongoStore = require('connect-mongo')(session);
+var MongoStore = require('connect-mongo/es5')(session);
 var passport = require('passport');
 
 
@@ -37,6 +38,8 @@ app.use(session({
   store: new MongoStore({ url: secret.database, autoReconnect: true})
 }));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
